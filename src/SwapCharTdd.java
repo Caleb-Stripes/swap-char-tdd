03713a232fb6fcb978ed122f2b0ca7359766bc40
Caleb-Stripes <calebdstripes@gmail.com>
2020-01-05 15:17:51 -0500

    new char test

---
+++ src/SwapCharTdd.java
@@ -0,0 +1,16 @@
+
+public class SwapCharTdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static String last2char(String str) {
+		// TODO Auto-generated method stub
+		int strlength = str.length();
+		
+		
+	}
+
+}
---
+++ src/SwapCharTddTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SwapCharTddTest {
+
+	@Test
+	void swapLast2(){
+		
+			assertEquals("", SwapCharTdd.last2char(""));
+		}
+	}
+
+
